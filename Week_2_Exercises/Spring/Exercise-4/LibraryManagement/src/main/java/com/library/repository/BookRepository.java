2b685c57d263f00e3adfca6fef5a448d02202ed2
Oshith Polamarasetti <132034763+oshithP@users.noreply.github.com>
2024-08-08 15:54:39 +0530

    third commit

---
+++ Week_2_Exercises/Spring/Exercise-4/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.library.repository;
+
+public class BookRepository {
+
+    // Example method to display book details
+    public void displayBooks() {
+        // Sample data or logic to display books
+        System.out.println("Displaying books from the repository..");
+        // In real application, this method would interact with database
+    }
+}
